08364c7a23a0bd1fed0e247f7becdefc8a6a7526
Ty Morris <tireymorris@gmail.com>
2019-05-10 16:18:02 -0500

    Add many matrix mutative operations including add, scale, multiply, increment, decrement, and subtract

--- spec/Matrix.spec.js
+++ spec/Matrix.spec.js
@@ -1,5 +1,9 @@
 import Matrix from '../src/Matrix';
 
+const expectAllValuesToPass = (matrix, expectation) => {
+  matrix.values.forEach(row => row.forEach(value => expectation(value)));
+};
+
 describe('Matrix', () => {
   describe('initializes', () => {
     test(' uses initial value and rows/columns', () => {
@@ -9,9 +13,64 @@ describe('Matrix', () => {
       );
 
       matrix = new Matrix({ rows: 2, columns: 2, initialValue: 5 });
-      matrix.values.forEach(row =>
-        row.forEach(value => expect(value).toEqual(5))
-      );
+      expectAllValuesToPass(matrix, value => expect(value).toEqual(5));
+      expect(matrix.values.length === matrix.rows);
+      expect(matrix.values[0].length === matrix.columns);
+    });
+  });
+
+  describe('add', () => {
+    test('adds matrices', () => {
+      const matrixOne = new Matrix({ rows: 3, columns: 3, initialValue: 2 });
+      const matrixTwo = new Matrix({ rows: 3, columns: 3, initialValue: 4 });
+
+      matrixOne.add(matrixTwo);
+
+      expectAllValuesToPass(matrixOne, value => expect(value).toEqual(6));
+    });
+  });
+
+  describe('decrement', () => {
+    test('decrements by a scalar', () => {
+      const matrixOne = new Matrix({ rows: 3, columns: 3, initialValue: 2 });
+
+      matrixOne.decrement(7);
+
+      expectAllValuesToPass(matrixOne, value => expect(value).toEqual(-5));
+    });
+  });
+
+  describe('entrywiseProduct', () => {
+    test('multiplies elements of both matrices', () => {
+      const matrixOne = new Matrix({ rows: 2, columns: 2, initialValue: 2 });
+      const matrixTwo = new Matrix({ rows: 2, columns: 2, initialValue: 2 });
+
+      matrixOne.entrywiseProduct(matrixTwo);
+
+      expectAllValuesToPass(matrixOne, value => expect(value).toEqual(4));
+    });
+  });
+
+  describe('increment', () => {
+    test('increments by a scalar', () => {
+      const matrixOne = new Matrix({ rows: 3, columns: 3, initialValue: 2 });
+
+      matrixOne.increment(7);
+
+      expectAllValuesToPass(matrixOne, value => expect(value).toEqual(9));
+    });
+  });
+
+  describe('randomize', () => {
+    test('generates random values', () => {
+      const matrix = new Matrix({ rows: 15, columns: 15 });
+
+      matrix.randomize();
+
+      expectAllValuesToPass(matrix, value => {
+        expect(value).toBeGreaterThanOrEqual(0);
+        expect(value).toBeLessThanOrEqual(9);
+      });
     });
   });
 
@@ -20,21 +79,30 @@ describe('Matrix', () => {
       let matrix = new Matrix({ rows: 3, columns: 3, initialValue: 9 });
 
       matrix.scale(3);
-      matrix.values.forEach(row =>
-        row.forEach(value => expect(value).toEqual(27))
-      );
+      expectAllValuesToPass(matrix, value => expect(value).toEqual(27));
+    });
+  });
+
+  describe('subtract', () => {
+    test('subtracts matrices', () => {
+      const matrixOne = new Matrix({ rows: 3, columns: 3, initialValue: 2 });
+      const matrixTwo = new Matrix({ rows: 3, columns: 3, initialValue: 4 });
+
+      matrixOne.subtract(matrixTwo);
+
+      expectAllValuesToPass(matrixOne, value => expect(value).toEqual(-2));
     });
   });
 
   describe('iterativelyApply', () => {
-    let matrix = new Matrix({ rows: 3, columns: 3, initialValue: 1 });
+    test('correctly sets values in matrix', () => {
+      let matrix = new Matrix({ rows: 3, columns: 3, initialValue: 1 });
 
-    matrix.iterativelyApply(
-      ([i, j]) => (matrix.values[i][j] = -matrix.values[i][j])
-    );
+      matrix.iterativelyApply(
+        ([i, j]) => (matrix.values[i][j] = -matrix.values[i][j])
+      );
 
-    matrix.values.forEach(row =>
-      row.forEach(value => expect(value).toEqual(-1))
-    );
+      expectAllValuesToPass(matrix, value => expect(value).toEqual(-1));
+    });
   });
 });
--- src/Matrix.js
+++ src/Matrix.js
@@ -8,6 +8,10 @@ class Matrix {
   }
 
   initialize(initialValue) {
+    if (typeof initialValue !== 'number') {
+      throw new Error('initial value must be a number');
+    }
+
     for (let i = 0; i < this.rows; i++) {
       this.values[i] = new Array(this.columns);
       for (var j = 0; j < this.columns; j++) {
@@ -16,11 +20,64 @@ class Matrix {
     }
   }
 
+  add(matrix) {
+    if (this.rows !== matrix.rows || this.columns !== matrix.columns) {
+      throw new Error('matrices being added must have same dimensions');
+    } else if (!matrix) {
+      throw new error('matrix cannot be undefined or null');
+    }
+
+    this.iterativelyApply(([i, j]) => this.values[i][j] + matrix.values[i][j]);
+  }
+
+  decrement(dec) {
+    if (typeof dec !== 'number') {
+      throw new Error('must decrement by a number');
+    }
+    this.iterativelyApply(([i, j]) => this.values[i][j] - dec);
+  }
+
+  entrywiseProduct(matrix) {
+    this.iterativelyApply(([i, j]) => this.values[i][j] * matrix.values[i][j]);
+  }
+
+  increment(inc) {
+    if (typeof inc !== 'number') {
+      throw new Error('must increment by a number');
+    }
+    this.iterativelyApply(([i, j]) => this.values[i][j] + inc);
+  }
+
+  randomize(ceiling = 10, floor = 0) {
+    if (typeof ceiling !== 'number' || typeof floor !== 'number') {
+      throw new Error('ceiling and floor must be numbers');
+    }
+    this.iterativelyApply(() => Math.floor(Math.random() * ceiling + floor));
+  }
+
+  subtract(matrix) {
+    if (this.rows !== matrix.rows || this.columns !== matrix.columns) {
+      throw new Error('matrices being subtracted must have same dimensions');
+    } else if (!matrix) {
+      throw new error('matrix cannot be undefined or null');
+    }
+
+    this.iterativelyApply(([i, j]) => this.values[i][j] - matrix.values[i][j]);
+  }
+
   scale(scalar) {
+    if (typeof scalar !== 'number') {
+      throw new Error('scalar must be a number');
+    }
+
     this.iterativelyApply(([i, j]) => this.values[i][j] * scalar);
   }
 
   iterativelyApply(operation) {
+    if (typeof operation !== 'function') {
+      throw new Error('operation must be a function');
+    }
+
     this.values = this.values.map((row, i) =>
       row.map((_, j) => operation([i, j]))
     );
